f580cf2ba60f4ebd8fd8e1a46c3ffa15f652a274
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2025-09-19 19:07:06 -0400

    Added player movement and singleton script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    CharacterController controller;
+
+    // Movement variables
+    Vector2 moveDirection = Vector2.zero;
+    [SerializeField] float moveSpeed = 5f;
+    
+    Transform cameraTransform;
+
+    // Input actions
+    public InputActionAsset InputActions;
+    InputAction move;
+
+    void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+    void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+
+        move = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+        Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y);
+        movement = cameraTransform.forward * movement.z + cameraTransform.right * movement.x;
+        movement.y = 0f;
+
+        controller.Move(movement * moveSpeed * Time.deltaTime);
+    }
+}
+
